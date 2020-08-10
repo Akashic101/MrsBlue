7bd003b6a328f29c9eb7577f9aa30589ac5b5451
David Moll <david.leander.moll@gmail.com>
2020-08-10 17:22:56 +0200

    Added valid-url-check

--- commands/addcouples.js
+++ commands/addcouples.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const Discord = require('discord.js');
 var pjson = require('../package.json');
+var validUrl = require('valid-url');
 
 const couplesSeq = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
@@ -50,10 +51,11 @@ module.exports = {
         if (!message.member.roles.cache.has(process.env.MOD_ROLE)) {
             return message.channel.send("I'm sorry, you do not have the permissions to do that. If you think this was a mistake please contact <@320574128568401920>")  
         }
-        else if (args.length != 2) {
-            message.channel.send("I'm sorry, it seems like you entered the command wrong. Please check if you entered it correcty or use !commands to see how your command should look like. If you believe there is an error, please contact <@320574128568401920>")
-            return
+
+        else if (args.length != 2 || !(validUrl.isUri(args[0])) || !(validUrl.isUri(args[1]))) {
+            return message.channel.send("I'm sorry, it seems like you entered the command wrong. Please check if you entered it correcty and the first link is from Imgur and the second from Instagram. If you believe there is an error, please contact <@320574128568401920>") 
         }
+
         else {
             try {
                 const add = await couples.create({
